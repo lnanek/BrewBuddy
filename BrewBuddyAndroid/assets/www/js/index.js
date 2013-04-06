c366bc32f6ab4d64705b2df99e2816f49aaea344
lance_nanek <lance_nanek@htc.com>
2013-04-06 12:49:18 -0700

    Mock up search results, mock up voting options, etc.

--- BrewBuddyAndroid/assets/www/js/index.js
+++ BrewBuddyAndroid/assets/www/js/index.js
@@ -14,13 +14,7 @@
     function onSuccess(position) {
         var element = document.getElementById('geolocation');
         element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          +                                   position.timestamp          + '<br />';
+                            'Longitude: '          + position.coords.longitude             + '<br />';
     }
 
     // onError Callback receives a PositionError object
